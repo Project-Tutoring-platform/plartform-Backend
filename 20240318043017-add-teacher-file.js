ee871d57c00eccbd75b63946b86a8ae71bcd2d84
Ash2700 <vmvuruvu@gmail.com>
2024-04-03 01:49:09 +0800

    migrate -8

--- 20240318043017-add-teacher-file.js
+++ 20240318043017-add-teacher-file.js
@@ -1,12 +1,13 @@
 'use strict'
 const faker = require('faker')
-const { User } = require('../models')
+const { User } = require('./models')
 
 const Teachers = User.findAll({ where: { isTeacher: true }, raw: true })
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await console.log(Teachers)
     await queryInterface.bulkInsert('Teachers',
       Array.from(await Teachers, (teacher) => ({
         teaching_style: faker.lorem.text(),

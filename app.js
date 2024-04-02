84869a7b3f51e921dd6f6aca85f963b82bd8f17c
Ash2700 <vmvuruvu@gmail.com>
2024-04-03 00:24:26 +0800

    migrateion

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-require('dotenv').config()
+if (process.env.NODE_ENV === 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const port = process.env.PORT || 3000
 const app = express()
 
 const router = require('./routes')
@@ -10,7 +13,7 @@ app.use(bodyParser.json())
 
 app.use(router)
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('express server running on http://platform-backend-dev.ap-northeast-1.elasticbeanstalk.com')
 })
 

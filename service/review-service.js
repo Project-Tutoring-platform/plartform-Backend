b99436d023f0cd6ca43706125f051412c581dc84
Ash2700 <vmvuruvu@gmail.com>
2024-04-22 11:37:19 +0800

    update readme

--- service/review-service.js
+++ service/review-service.js
@@ -1,14 +1,13 @@
 const {  Review } = require('../models')
 
-
 const reviewService = {
-  postReview: req => {
-    const { text, point, } = req.body
-    const id = req.query
-    Review.findByPk(id)
-    .then(review => {
-      if(review) 
-    })
-  }
+  // postReview: req => {
+  //   const { text, point, } = req.body
+  //   const id = req.query
+  //   Review.findByPk(id)
+  //   .then(review => {
+  //     if(review) 
+  //   })
+  // }
 }
 module.exports = reviewService
--- testdayjsisAfter.js
+++
@@ -1,3 +0,0 @@
-const dayjs = require('dayjs')
-const  isAfter = dayjs('2024-04-19 00:00').isAfter(dayjs('2024-04-20 11:30'))
-console.log(isAfter)
